e9cb5fd748bd027c647f546bc1c6d63ffec81ec3
sh2dow <carbonat87@gmail.com>
2017-02-03 16:19:45 +0200

    Initial

---
+++ WebFileManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebFileManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebFileManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFileManager.WebApiApplication" Language="C#" %>
