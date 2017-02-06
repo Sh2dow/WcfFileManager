511f411378562d1040fd343c3945facd5d2d2278
sh2dow <sh2dow@carbonat87@gmail.com>
2017-02-06 23:25:59 +0200

    Angular presentation layer

--- WebApplication/Scripts/app/Angularapp.js
+++ WebApplication/Scripts/app/Angularapp.js
@@ -13,15 +13,64 @@
     function fsitems(fsManager) {
         /* jshint validthis:true */
         var vm = this;
-        vm.title = 'fs manager';
+        vm.title = 'FileManager';
         vm.fsitems = fsManager.fsitems;
-        vm.get = fsManager.get;
+        vm.load = fsManager.load;
+        vm.remove = fsManager.remove;
 
-        get('');
+        //get('');
 
-        function get(path) {
-            var f = fsManager.get(path)
+        activate();
+
+        function activate() {
+            fsManager.load();
         }
+    }
+
+    function edit(fsitem) {
+        appInfo.setInfo({ busy: true, message: "editing fsitem " + fsitem.name });
+        return fsitemManagerClient.edit({ fileName: fsitem.name })
+                                    .$promise
+                                    .then(function (result) {
+                                        //if the fsitem was edited
+                                        var i = service.fsitems.indexOf(fsitem);
+                                        service.fsitems.splice(i, 1);
+
+                                        appInfo.setInfo({ message: "fsitems edit" });
+
+                                        return result.$promise;
+                                    },
+                                    function (result) {
+                                        appInfo.setInfo({ message: "something went wrong: " + result.data.message });
+                                        return $q.reject(result);
+                                    })
+                                    ['finally'](
+                                    function () {
+                                        appInfo.setInfo({ busy: false });
+                                    });
+    }
+
+    function remove(fsitem) {
+        appInfo.setInfo({ busy: true, message: "deleting fsitem " + fsitem.name });
+        return fsitemManagerClient.remove({ fileName: fsitem.name })
+                                    .$promise
+                                    .then(function (result) {
+                                        //if the fsitem was deleted successfully remove it from the fsitems array
+                                        var i = service.fsitems.indexOf(fsitem);
+                                        service.fsitems.splice(i, 1);
+
+                                        appInfo.setInfo({ message: "fsitems deleted" });
+
+                                        return result.$promise;
+                                    },
+                                    function (result) {
+                                        appInfo.setInfo({ message: "something went wrong: " + result.data.message });
+                                        return $q.reject(result);
+                                    })
+                                    ['finally'](
+                                    function () {
+                                        appInfo.setInfo({ busy: false });
+                                    });
     }
 
     angular
@@ -33,12 +82,14 @@
     function fsManager($q, fsManagerClient) {
         var service = {
             fsitems: [],
-            get: get
+            load: load,
+            edit: edit,
+            remove: remove
         };
 
         return service;
 
-        function get(path) {
+        function load(path) {
             console.log(path);
             service.fsitems.length = 0;
             return fsManagerClient.get({ path: path })
@@ -60,23 +111,14 @@
 
     fsManagerClient.$inject = ['$resource'];
 
-    //function fsManagerClient($resource) {
-    //    return $resource("http://localhost:1786/FileService.svc", { path: "@path" },
-    //            {
-    //                get: { method: 'GET', isArray: true },
-    //                query: { method: 'GET', isArray: true },
-    //                save: { method: 'POST', transformRequest: angular.identity, headers: { 'Content-Type': undefined } },
-    //                remove: { method: 'DELETE', url: 'api/photo/:fileName', params: { name: '@fileName' } }
-    //            });
-    //}
-
     function fsManagerClient($resource) {
         return $resource("http://localhost:1786/FileService.svc/GetAllFiles?:path", { path: "@path" },
                 {
-                    get: { method: 'GET', isArray: true },
-                    save: { method: 'POST', transformRequest: angular.identity, headers: { 'Content-Type': undefined } },
-                    edit: { method: 'PUT' },
-                    remove: { method: 'DELETE', params: { name: '@fileName' } }
+                    //'get': { method: 'GET', isArray: true },
+                    'get': { method: 'GET', url: 'http://localhost:1786/FileService.svc/GetAllFiles/:fileName', isArray: true, params: { path: '@path' } },
+                    'save': { method: 'POST', url: 'http://localhost:1786/FileService.svc/Delete/:fileName', transformRequest: angular.identity, headers: { 'Content-Type': undefined }, params: { name: '@fileName' } },
+                    'edit': { method: 'PUT', url: 'http://localhost:1786/FileService.svc/PUT/:fileName', params: { name: '@fileName' } },
+                    'remove': { method: 'DELETE', url: 'http://localhost:1786/FileService.svc/Delete/:fileName', params: { name: '@fileName' } }
                 });
     }
 
